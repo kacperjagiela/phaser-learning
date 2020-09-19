cc7229266e4f1c2aba90f591ceac6883591076f9
kacperjagiela <jagielakacper80@gmail.com>
2020-09-19 21:28:54 +0200

    env: initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import * as Phaser from 'phaser';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Learning',
+    type: Phaser.AUTO,
+
+    scale: {
+        width: window.innerWidth,
+        height: window.innerWidth,
+    },
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        },
+    },
+
+    parent: 'game',
+    backgroundColor: '#000000',
+};
+
+export const game = new Phaser.Game(gameConfig);
