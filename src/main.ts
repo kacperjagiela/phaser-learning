3e3dc288b4efff2f031c88c2378121ba8bb4a750
kacperjagiela <jagielakacper80@gmail.com>
2020-09-20 23:26:52 +0200

    feat: add time based coin generation and timer

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,34 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
     private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
-    private coins: Phaser.Physics.Arcade.StaticGroup;
+    private coins: Phaser.Physics.Arcade.Group;
     private scoreText: Phaser.GameObjects.Text;
+    private timeText: Phaser.GameObjects.Text;
+    private seconds: number;
     private score: number = 0;
+    private coinGeneratingConfig: Phaser.Types.Time.TimerEventConfig = {
+        delay: 1000,
+        loop: true,
+        callback: this.generateCoin,
+        callbackScope: this,
+    };
 
     constructor() {
         super(sceneConfig);
+    }
+
+    public generateCoin() {
+        this.coins.add(
+            this.add.circle(Math.random() * 10 * 100, Math.random() * 10 * 100, 50, 0xff0000),
+        );
+    }
+
+    public secondsToMinutes(seconds) {
+        return (
+            Phaser.Math.FloorTo(seconds / 60) +
+            ':' +
+            ('0' + Phaser.Math.FloorTo(seconds % 60)).slice(-2)
+        );
     }
 
     public collectCoin(player, coin) {
@@ -25,19 +47,22 @@ export class GameScene extends Phaser.Scene {
 
     public create() {
         this.square = this.add.rectangle(400, 400, 100, 100, 0xffffff) as any;
-        this.coins = this.physics.add.staticGroup();
-        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: 'red' });
+        // generating coins
+        this.coins = this.physics.add.group();
+        this.coinGeneratingConfig = this.time.addEvent(this.coinGeneratingConfig);
 
-        // Generate coins
-        for (let i = 1; i <= 5; i++) {
-            this.coins.add(this.add.circle(i * 100, i * 50, 50, 0x345345));
-        }
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: 'red' });
+        this.timeText = this.add.text(16, 80, 'Ticks: 0', { fontSize: '32px', fill: 'red' });
 
         this.physics.add.existing(this.square);
         this.physics.add.overlap(this.square, this.coins, this.collectCoin, null, this);
     }
 
     public update() {
+        // displaying time
+        this.seconds = Phaser.Math.CeilTo(this.time.now / 1000);
+        this.timeText.setText('Ticks: ' + this.secondsToMinutes(this.seconds));
+
         // movement
         const cursorKeys = this.input.keyboard.createCursorKeys();
 
